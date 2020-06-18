1a809458878e4570e771cb551bce9dab7e03777e
Soorajm100 <soorajm825@gmail.com>
2020-06-18 11:42:33 +0530

    cpp file
    no desc

---
+++ file1.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include<iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    char c='w';
+    int r=5;
+    float t=2.5;
+    double f=2.3654;
+    cout<<"The size of the char:"<<sizeof(c)<<endl;
+    cout<<"The size of the int:"<<sizeof(r)<<endl;
+    cout<<"The size of the float:"<<sizeof(t)<<endl;
+    cout<<"The size of the double:"<<sizeof(f)<<endl;
+    
+
+    return 0;
+}
+
